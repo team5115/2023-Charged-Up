df1414892e0faf4b7f853e40ddb4234c8fa8b5d3
Jonathan <deltagreen1990na@gmail.com>
2023-03-13 16:39:17 -0400

    trajectoy testing start

--- src/main/java/frc/team5115/Classes/Software/PhotonVision.java
+++ src/main/java/frc/team5115/Classes/Software/PhotonVision.java
@@ -15,14 +15,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team5115.Constants.*;
 
 public class PhotonVision extends SubsystemBase{
-    private PhotonCamera photonCameraL;
-    private PhotonCamera photonCameraR;
-    private PhotonPoseEstimator photonPoseEstimatorL;
-    private PhotonPoseEstimator photonPoseEstimatorR;
+    // private PhotonCamera photonCameraL;
+    // private PhotonCamera photonCameraR;
+    // private PhotonPoseEstimator photonPoseEstimatorL;
+    // private PhotonPoseEstimator photonPoseEstimatorR;
 
     public PhotonVision() {
-        photonCameraL = new PhotonCamera(VisionConstants.leftCameraName);
-        photonCameraR = new PhotonCamera(VisionConstants.rightCameraName);
+        // photonCameraL = new PhotonCamera(VisionConstants.leftCameraName);
+        // photonCameraR = new PhotonCamera(VisionConstants.rightCameraName);
         ArrayList<AprilTag> aprilTagList = new ArrayList<AprilTag>();
 
         // Add all the april tags
@@ -42,16 +42,16 @@ public class PhotonVision extends SubsystemBase{
         aprilTagList.add(GenerateAprilTag(5, -7.908830, +2.741613, 000));
 
         AprilTagFieldLayout fieldLayout = new AprilTagFieldLayout(aprilTagList, FieldConstants.length, FieldConstants.width);
-        photonPoseEstimatorL = new PhotonPoseEstimator(fieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, photonCameraL, VisionConstants.robotToCamL);
-        photonPoseEstimatorR = new PhotonPoseEstimator(fieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, photonCameraR, VisionConstants.robotToCamR);
+        // photonPoseEstimatorL = new PhotonPoseEstimator(fieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, photonCameraL, VisionConstants.robotToCamL);
+        // photonPoseEstimatorR = new PhotonPoseEstimator(fieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, photonCameraR, VisionConstants.robotToCamR);
     }
 
     public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
         // The teeam assignment of the first grid the robot looks at is the team assignment of the robot
         // otherwise if we cant see any april tags trust the team assignment inputted on shuffle board
         //Trusting the left camera more, no idea on how to use filters to get the most information out of both cameras 2-6-2022
-        if(photonPoseEstimatorL.update().isPresent()) return photonPoseEstimatorL.update();
-        if(photonPoseEstimatorR.update().isPresent()) return photonPoseEstimatorR.update();
+        // if(photonPoseEstimatorL.update().isPresent()) return photonPoseEstimatorL.update();
+        // if(photonPoseEstimatorR.update().isPresent()) return photonPoseEstimatorR.update();
         return Optional.empty();
     }
 
--- src/main/java/frc/team5115/Commands/Auto/AutoCommandGroup.java
+++ src/main/java/frc/team5115/Commands/Auto/AutoCommandGroup.java
@@ -25,25 +25,25 @@ public class AutoCommandGroup extends SequentialCommandGroup {
 
     private void setupIdeal() {
         addCommands(
-            new DriveForward(drivetrain, -1.0, 0.5),
-            new TestingPrint("done: back a little"),
-            new DriveForward(drivetrain, +3.0, 1.2),
-            new TestingPrint("done: fast forward"),
-            new DriveForward(drivetrain, +3.0, 0.6),
-            new TestingPrint("done: over ramp and exit community"),
-            new DockCommandGroup(drivetrain, true),
-            new TestingPrint("done: docking \n ----Auto Complete----")
+            new FollowTrajectory(drivetrain, 0.1, 0.1, 0.1)
+            // new TestingPrint("done: back up a little"),
+            // new FollowTrajectory(drivetrain, +3.0, 1.2, 1),
+            // new TestingPrint("done: fast forward"),
+            // new FollowTrajectory(drivetrain, +3.0, 0.6, 1),
+            // new TestingPrint("done: over ramp and exit community"),
+            // new DockCommandGroup(drivetrain, true),
+            // new TestingPrint("done: docking \n ----Auto Complete----")
 
         );
     }
 
     private void setupNotIdeal() {
         addCommands(
-            new DriveForward(drivetrain, -1.0, 0.5),
-            new TestingPrint("done: back a little"),
-            new DriveForward(drivetrain, +1.0, 1.2),
+            new FollowTrajectory(drivetrain, -1.0, 0.5, 0.1),
+            new TestingPrint("done: back up a little"),
+            new FollowTrajectory(drivetrain, +1.0, 1.2, 0.1),
             new TestingPrint("done: fast forward"),
-            new DriveForward(drivetrain, +5.0, 1),
+            new FollowTrajectory(drivetrain, +5.0, 1, 0.1),
             new TestingPrint("done: exit community")
         );
     }
