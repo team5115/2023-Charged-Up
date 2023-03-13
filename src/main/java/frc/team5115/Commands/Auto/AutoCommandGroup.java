1100cee9ec6b3c40e1a1ba94fb51eb88a53f5845
Gili Reeves <107364066+AvidCoder27@users.noreply.github.com>
2023-03-13 16:32:36 -0400

    merge autocommand group

--- src/main/java/frc/team5115/Commands/Auto/AutoCommandGroup.java
+++ src/main/java/frc/team5115/Commands/Auto/AutoCommandGroup.java
@@ -26,25 +26,19 @@ public class AutoCommandGroup extends SequentialCommandGroup {
     private void setupIdeal() {
         addCommands(
             new FollowTrajectory(drivetrain, 0.1, 0.1, 0.1)
-            // new TestingPrint("done: back up a little"),
-            // new FollowTrajectory(drivetrain, +3.0, 1.2, 1),
-            // new TestingPrint("done: fast forward"),
-            // new FollowTrajectory(drivetrain, +3.0, 0.6, 1),
-            // new TestingPrint("done: over ramp and exit community"),
-            // new DockCommandGroup(drivetrain, true),
-            // new TestingPrint("done: docking \n ----Auto Complete----")
-
+            // new DriveForward(drivetrain, -0.33, 0.5), // back up to node
+            // new DriveForward(drivetrain, +0.65, 1.2), // speed away to drop cube
+            // new DriveForward(drivetrain, +3.0, 0.6), // go over ramp
+            // new DriveForward(drivetrain, +0.5, 0.8), // exit community
+            // new DockCommandGroup(drivetrain, true) // dock backwards
         );
     }
 
     private void setupNotIdeal() {
         addCommands(
-            new FollowTrajectory(drivetrain, -1.0, 0.5, 0.1),
-            new TestingPrint("done: back up a little"),
-            new FollowTrajectory(drivetrain, +1.0, 1.2, 0.1),
-            new TestingPrint("done: fast forward"),
-            new FollowTrajectory(drivetrain, +5.0, 1, 0.1),
-            new TestingPrint("done: exit community")
+            new DriveForward(drivetrain, -0.33, 0.5), // back up to node
+            new DriveForward(drivetrain, +0.65, 1.2), // speed away to drop cube
+            new DriveForward(drivetrain, +3.5, 1.0) // go over ramp
         );
     }
 }
