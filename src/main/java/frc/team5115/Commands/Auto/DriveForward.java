7aca5e6827932355066ac3437c541722bd220a7c
Gili Reeves <107364066+AvidCoder27@users.noreply.github.com>
2023-03-13 16:31:14 -0400

    small edit to driveforward

--- src/main/java/frc/team5115/Commands/Auto/DriveForward.java
+++ src/main/java/frc/team5115/Commands/Auto/DriveForward.java
@@ -1,6 +1,5 @@
 package frc.team5115.Commands.Auto;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team5115.Classes.Software.Drivetrain;
 
@@ -10,6 +9,7 @@ public class DriveForward extends CommandBase{
     private double startRightDist;
     private double startleftDist;
     private double speed;
+    private boolean doneMoving;
 
     public DriveForward(Drivetrain drivetrain, double dist, double speed) {
         this.dist = dist;
@@ -24,13 +24,17 @@ public class DriveForward extends CommandBase{
     }
 
     @Override
+    public void execute() {
+        doneMoving = drivetrain.UpdateMoving(dist, startleftDist, startRightDist, speed);
+    }
+
+    @Override
     public void end(boolean interrupted) {
         drivetrain.stop();
     }
 
     @Override
     public boolean isFinished() {
-        boolean doneMoving = drivetrain.UpdateMoving(dist, startleftDist, startRightDist, speed);
         return doneMoving;
     }
 }
