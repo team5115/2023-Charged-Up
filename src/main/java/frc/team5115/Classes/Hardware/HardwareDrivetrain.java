6892dd6d89f92523027ca0d54af9f8d2abdb7a92
Delta-i <89542300+Delta-i@users.noreply.github.com>
2023-03-15 08:42:37 -0400

    Update HardwareDrivetrain.java

--- src/main/java/frc/team5115/Classes/Hardware/HardwareDrivetrain.java
+++ src/main/java/frc/team5115/Classes/Hardware/HardwareDrivetrain.java
@@ -29,13 +29,14 @@ public class HardwareDrivetrain{
     // // END of comp robot values
 
     // Testbed feedforward and feedback (pid) values - 6 inch diameter on testbed
-    private final double leftKs = 0.090949;
+    /* private final double leftKs = 0.090949;
     private final double leftKv = 2.783;
     private final double leftKa = 0.16477;
     
     private final double rightKs = 0.099706;
     private final double rightKv = 2.8314;
     private final double rightKa = 0.14565;
+    */
 
     private final double leftKp = 0; // 3.7203 according to sysid
     private final double rightKp = 0; // 3.7203 according to sysid
