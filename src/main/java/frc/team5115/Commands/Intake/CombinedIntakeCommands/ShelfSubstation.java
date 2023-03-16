4436fb9c219f486734d1677b5c4b62a9e97be1f6
dekta <deltagreen1990na@gmail.com>
2023-03-16 16:54:55 -0400

    up up up uppity substation

--- src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/ShelfSubstation.java
+++ src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/ShelfSubstation.java
@@ -13,7 +13,7 @@ public class ShelfSubstation extends SequentialCommandGroup {
         this.intake = intake;
         addCommands(
             new RealExtend(intake, 0),
-            new IntakeTurn(intake, 13.2),
+            new IntakeTurn(intake, 14),
             new RealExtend(intake, 0)
         );
     }
