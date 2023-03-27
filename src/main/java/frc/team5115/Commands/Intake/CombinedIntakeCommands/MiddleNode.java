4bd60726aadb7c9d53d60dc9d397c94b07cbacb5
AvidCoder27 <gilireeves@gmail.com>
2023-03-27 17:37:05 -0400

    wrapped node commands for proper init

--- src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/MiddleNode.java
+++ src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/MiddleNode.java
@@ -1,34 +1,46 @@
 package frc.team5115.Commands.Intake.CombinedIntakeCommands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.team5115.Classes.Hardware.HardwareIntake;
 import frc.team5115.Classes.Software.Arm;
 import frc.team5115.Commands.Intake.RealExtend;
-import frc.team5115.Commands.Intake.RawIntakeCommands.IntakeExtend_v2;
 import frc.team5115.Commands.Intake.RawIntakeCommands.IntakeTurn;
 
-public class MiddleNode extends SequentialCommandGroup {
+public class MiddleNode extends CommandBase {
     Arm intake;
+    WrappedMiddleNode wrappedMiddleNode;
 
     public MiddleNode(Arm intake){
         this.intake = intake;
-        final HardwareIntake claw = intake.h;
-        boolean cubeMode = !claw.open;
+    }
 
-        double length = 8;
-        double angle = 10;
+    @Override
+    public void initialize() {
+        wrappedMiddleNode = new WrappedMiddleNode(intake);
+        wrappedMiddleNode.schedule();
+    }
 
-        if (cubeMode) {
-            angle -= 1;
+    @Override
+    public final boolean isFinished() {
+        return wrappedMiddleNode.isFinished();
+    }
+
+    private class WrappedMiddleNode extends SequentialCommandGroup {
+        public WrappedMiddleNode(Arm arm) {
+            boolean cubeMode = !arm.h.open;
+
+            double length = 8;
+            double angle = 10;
+
+            if (cubeMode) {
+                angle -= 1;
+            }
+
+            addCommands(
+                new RealExtend(intake, 0),
+                new IntakeTurn(intake, angle),
+                new RealExtend(intake, length)
+            );
         }
-
-        addCommands(
-            new RealExtend(intake, 0),
-            new IntakeTurn(intake, angle),
-            new RealExtend(intake, length)
-            //, new IntakeTurn(intake, 5) not getting implemented until arm has consistent tensioning b/c this can cause nicking & arm lowering before needed driver input
-            //you cannot say one day testing where this code works disproves this claim because the tensioning changes between days, and that is why driver input is needed
-            // to correct this 
-        );
     }
 }
