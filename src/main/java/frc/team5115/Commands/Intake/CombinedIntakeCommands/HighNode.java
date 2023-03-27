8de6c55b1e97407ddb40f14281b06f2abb6e8d1a
Gili Reeves <107364066+AvidCoder27@users.noreply.github.com>
2023-03-27 16:24:05 -0400

    different setpoints for cube

--- src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/HighNode.java
+++ src/main/java/frc/team5115/Commands/Intake/CombinedIntakeCommands/HighNode.java
@@ -1,6 +1,7 @@
 package frc.team5115.Commands.Intake.CombinedIntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team5115.Classes.Hardware.HardwareIntake;
 import frc.team5115.Classes.Software.Arm;
 import frc.team5115.Commands.Intake.RealExtend;
 import frc.team5115.Commands.Intake.RawIntakeCommands.IntakeExtend_v2;
@@ -11,10 +12,21 @@ public class HighNode extends SequentialCommandGroup {
 
     public  HighNode(Arm intake){
         this.intake = intake;
+        final HardwareIntake claw = intake.h;
+        boolean cubeMode = !claw.open;
+
+        double length = 25;
+        double angle = 20;
+
+        if (cubeMode) {
+            length -= 7;
+            angle -= 2;
+        }
+
         addCommands(
             new RealExtend(intake, 0),
-            new IntakeTurn(intake, 20),
-            new RealExtend(intake, 25)
+            new IntakeTurn(intake, angle),
+            new RealExtend(intake, length)
             //, new IntakeTurn(intake, 15) not getting implemented until arm has consistent tensioning b/c this can cause nicking & arm lowering before needed driver input
             //you cannot say one day testing where this code works disproves this claim because the tensioning changes between days, and that is why driver input is needed
             // to correct this 
