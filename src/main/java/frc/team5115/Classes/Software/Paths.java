87a758477a83b2161cf1de6464cf399d9698daa9
Gili Reeves <107364066+AvidCoder27@users.noreply.github.com>
2023-04-27 14:55:25 -0400

    move paths to constructor

--- src/main/java/frc/team5115/Classes/Software/Paths.java
+++ src/main/java/frc/team5115/Classes/Software/Paths.java
@@ -1,14 +1,28 @@
 package frc.team5115.Classes.Software;
+import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Paths extends SubsystemBase {
-    public final PathPlannerTrajectory SideAutoPt1 = PathPlanner.loadPath("Side Auto Pt. 1", null);
-	public final PathPlannerTrajectory SideAutoPt2 = PathPlanner.loadPath("Side Auto Pt. 2", null);
-	public final PathPlannerTrajectory SideAutoPt3 = PathPlanner.loadPath("Side Auto Pt. 3", null);
-	public final PathPlannerTrajectory SideAuto = PathPlanner.loadPath("Side Auto", null);
-	public final PathPlannerTrajectory ScoreHighWithDock = PathPlanner.loadPath("Score High With Dock", null);
-	public final PathPlannerTrajectory ExitCommunity = PathPlanner.loadPath("Exit Community", null);
+    private final double MaxSpeed = 0.1; // m/s
+    private final double MaxAcceleration = 0.1; // m/s^2
+    public final PathConstraints constraints = new PathConstraints(MaxSpeed, MaxAcceleration);
+
+    public final PathPlannerTrajectory SideAutoPt1;
+	public final PathPlannerTrajectory SideAutoPt2;
+	public final PathPlannerTrajectory SideAutoPt3;
+	public final PathPlannerTrajectory SideAuto;
+	public final PathPlannerTrajectory ScoreHighWithDock;
+	public final PathPlannerTrajectory ExitCommunity;
+
+    public Paths() {
+        SideAutoPt1 = PathPlanner.loadPath("Side Auto Pt. 1", constraints);
+        SideAutoPt2 = PathPlanner.loadPath("Side Auto Pt. 2", constraints);
+        SideAutoPt3 = PathPlanner.loadPath("Side Auto Pt. 3", constraints);
+        SideAuto = PathPlanner.loadPath("Side Auto", constraints);
+        ScoreHighWithDock = PathPlanner.loadPath("Score High With Dock", constraints);
+        ExitCommunity = PathPlanner.loadPath("Exit Community", constraints);
+    }
 }
