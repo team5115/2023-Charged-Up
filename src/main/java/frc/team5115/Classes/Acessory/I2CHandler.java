e86662de6b513d25ed20e43c1b3b293b03777ead
Jonathan <deltagreen1990na@gmail.com>
2023-05-18 16:03:53 -0400

    trying to fix it :(

--- src/main/java/frc/team5115/Classes/Acessory/I2CHandler.java
+++ src/main/java/frc/team5115/Classes/Acessory/I2CHandler.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team5115.Classes.Hardware.NAVx;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.I2C.Port;
+import java.util.Arrays;
 
 public class I2CHandler extends SubsystemBase {
     I2C i2c;
@@ -11,9 +12,16 @@ public class I2CHandler extends SubsystemBase {
     static final byte rollAddress = 0x1C;
     static final byte pitchAddress = 0x1E;
     static final byte yawAddress = 0x1A;
+
+    static final byte xGravAddress = 0x2E;
+    static final byte yGravAddress = 0x30;
+    static final byte zGravAddress = 0x32;
     short lastPitch;
     short lastRoll;
     short lastYaw;
+    short lastXgrav;
+    short lastYgrav;
+    short lastZgrav;
 
     public I2CHandler() {
         i2c = new I2C(Port.kMXP, 0x28);
@@ -21,7 +29,7 @@ public class I2CHandler extends SubsystemBase {
         buffer = new byte[2];
     }
 
-    public static short combineBytes(byte msb, byte lsb) {
+    public static short combineBytes(byte lsb, byte msb) {
         return (short)((msb << 8) | (lsb & 0xFF));
     }
 
@@ -29,7 +37,26 @@ public class I2CHandler extends SubsystemBase {
         if (bytes.length == 1) {
             return (short) bytes[0];
         }
-        return combineBytes(bytes[1], bytes[0]);
+        return combineBytes(bytes[0], bytes[1]);
+    }
+
+    public static double[] vectorNormalize(double[] v) {
+        double[] result = new double[v.length];
+        double sum = 0;
+        for (double d : v) {
+            sum += d;
+        }
+        for (int i = 0; i < v.length; i++) {
+            result[i] = v[i] / sum;
+        }
+        return result;
+    }
+
+    private double[] getGravity() {
+        lastXgrav = readFromSensor(xGravAddress, 2, lastXgrav);        
+        lastYgrav = readFromSensor(yGravAddress, 2, lastYgrav);        
+        lastZgrav = readFromSensor(zGravAddress, 2, lastZgrav);   
+        return vectorNormalize(new double[] {lastXgrav, lastYgrav, lastZgrav});
     }
 
     private short getPitch() {
@@ -49,7 +76,8 @@ public class I2CHandler extends SubsystemBase {
     }
 
     public double getPitchReal() {
-        return NAVx.clampAngle((double) getYaw() / 16.0 - 63.777);
+        System.out.println("Gravity: " + Arrays.toString(getGravity()));
+        return NAVx.clampAngle((double) getYaw() / 16.0);
     }
 
     private short readFromSensor(byte registerAddress, int count, short defaultValue) {
@@ -59,7 +87,7 @@ public class I2CHandler extends SubsystemBase {
             System.out.println("Failed to read from BNO055");
             return defaultValue;
         }
-        return combineBytes(buffer); // 3500 = down, 4800 = horizontal
+        return combineBytes(buffer); 
     }
 
     public void Disable() {
